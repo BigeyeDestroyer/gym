014030e4391d30cd152982db130e14f824c68a9c
BigeyeDestroyer <lurui08@163.com>
2016-05-08 19:32:17 +0800

    add code interpretation

--- toy_examples/cart-pole.py
+++ toy_examples/cart-pole.py
@@ -1,6 +1,22 @@
 import gym
-env = gym.make('MsPacman-v0')
-env.reset()
-for _ in xrange(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+env = gym.make('CartPole-v0')
+for i_episode in xrange(5):
+    observation = env.reset()
+    for t in xrange(100):
+        env.render()
+        print observation
+        action = env.action_space.sample()
+        """
+        observation (object) : an environment-specific object
+                               representing your observation
+                               of the environment
+        reward (float)       : amount of reward achieved by the previous
+                               action
+        done (boolean)       : whether it's time to reset environment
+                               again
+        info (dict)          : diagnostic information useful for debugging
+        """
+        observation, reward, done, info = env.step(action)
+        if done:
+            print "Episode finished after {} timesteps".format(t+1)
+            break
